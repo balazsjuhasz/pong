a8ac38226e7203ead064f86275cd9ff777bb4561
Balazs Juhasz <juhaszbal@gmail.com>
2021-02-07 15:53:03 +0100

    comment back canvas drawing

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Canvas
 const { body } = document;
-
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
 const width = 500;
 const height = 700;
 const screenWidth = window.screen.width;
@@ -47,65 +48,71 @@ const winningScore = 7;
 // let isNewGame = true;
 
 // Render Everything on Canvas
-function renderCanvas() {
+const renderCanvas = () => {
   // Canvas Background
-  // context.fillStyle = 'black';
-  // context.fillRect(0, 0, width, height);
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
+
   // Paddle Color
-  // context.fillStyle = 'white';
+  context.fillStyle = 'white';
+
   // Player Paddle (Bottom)
-  // context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+  context.fillRect(paddleBottomX, height - 20, paddleWidth, paddleHeight);
+
   // Computer Paddle (Top)
-  // context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+  context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+
   // Dashed Center Line
-  // context.beginPath();
-  // context.setLineDash([4]);
-  // context.moveTo(0, 350);
-  // context.lineTo(500, 350);
-  // context.strokeStyle = 'grey';
-  // context.stroke();
+  context.beginPath();
+  context.setLineDash([4]);
+  context.moveTo(0, 350);
+  context.lineTo(500, 350);
+  context.strokeStyle = 'grey';
+  context.stroke();
+
   // Ball
-  // context.beginPath();
-  // context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
-  // context.fillStyle = 'white';
-  // context.fill();
+  context.beginPath();
+  context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
+  context.fillStyle = 'white';
+  context.fill();
+
   // Score
-  // context.font = '32px Courier New';
-  // context.fillText(playerScore, 20, canvas.height / 2 + 50);
-  // context.fillText(computerScore, 20, canvas.height / 2 - 30);
-}
+  context.font = '32px Courier New';
+  context.fillText(playerScore, 20, canvas.height / 2 + 50);
+  context.fillText(computerScore, 20, canvas.height / 2 - 30);
+};
 
 // Create Canvas Element
-function createCanvas() {
+const createCanvas = () => {
   canvas.width = width;
   canvas.height = height;
   body.appendChild(canvas);
   renderCanvas();
-}
+};
 
 // Remove this
 createCanvas();
 
 // Reset Ball to Center
-function ballReset() {
+const ballReset = () => {
   ballX = width / 2;
   ballY = height / 2;
   speedY = -3;
   paddleContact = false;
-}
+};
 
 // Adjust Ball Movement
-function ballMove() {
+const ballMove = () => {
   // Vertical Speed
   ballY += -speedY;
   // Horizontal Speed
   if (playerMoved && paddleContact) {
     ballX += speedX;
   }
-}
+};
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
-function ballBoundaries() {
+const ballBoundaries = () => {
   // Bounce off Left Wall
   if (ballX < 0 && speedX < 0) {
     speedX = -speedX;
@@ -154,10 +161,10 @@ function ballBoundaries() {
       playerScore++;
     }
   }
-}
+};
 
 // Computer Movement
-function computerAI() {
+const computerAI = () => {
   if (playerMoved) {
     if (paddleTopX + paddleDiff < ballX) {
       paddleTopX += computerSpeed;
@@ -165,9 +172,9 @@ function computerAI() {
       paddleTopX -= computerSpeed;
     }
   }
-}
+};
 
-function showGameOverEl(winner) {
+const showGameOverEl = (winner) => {
   // Hide Canvas
   // // Container
   // gameOverEl.textContent = '';
@@ -180,28 +187,28 @@ function showGameOverEl(winner) {
   // playAgainBtn.setAttribute('onclick', 'startGame()');
   // playAgainBtn.textContent = 'Play Again';
   // // Append
-}
+};
 
 // Check If One Player Has Winning Score, If They Do, End Game
-function gameOver() {
+const gameOver = () => {
   // if (playerScore === winningScore || computerScore === winningScore) {
   //   isGameOver = ;
   //   // Set Winner
   //   let winner = ;
   //   showGameOverEl(winner);
   // }
-}
+};
 
 // Called Every Frame
-function animate() {
+const animate = () => {
   renderCanvas();
   ballMove();
   ballBoundaries();
   computerAI();
-}
+};
 
 // Start Game, Reset Everything
-function startGame() {
+const startGame = () => {
   // if (isGameOver && !isNewGame) {
 
   // }
@@ -226,7 +233,7 @@ function startGame() {
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
-}
+};
 
 // On Load
 // startGame();
